ed85e347a738de4f9f8621d13480b7f0e51970f9
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2015-11-05 21:14:05 +0200

    So Ember-CLI "fixes" indentation on router.js every time new route is generated

--- app/router.js
+++ app/router.js
@@ -2,15 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-	location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-	this.route('articles', function () {
-		this.route('article', {path: ':article_id'});
-	});
-	this.route('about');
-	this.route('snippets');
+    this.route('articles', function () {
+        this.route('article', {path: ':article_id'});
+    });
+    this.route('about');
+    this.route('snippets');
 
 });
 

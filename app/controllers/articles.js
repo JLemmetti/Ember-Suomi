1ebafc8b01d0b2ee46c4901be08d1c1240dfced8
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2015-12-16 22:36:29 +0200

    Sort articles by release date

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	sortParams: ['published:asc'],
+	sorted: Ember.computed.sort('model', 'sortParams'),
+
 	actions: {
 		deleteArticle (article) {
 			article.deleteRecord();

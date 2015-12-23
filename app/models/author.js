f7778229c469445de464ec73399fef76e4de1459
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2015-12-23 19:20:51 +0200

    Fixed typo

--- app/models/author.js
+++ app/models/author.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	// articles: hasMany('article'),
+	articles: DS.hasMany('article'),
 
 	name: DS.attr('string'),
 	twitter: DS.attr('string'),

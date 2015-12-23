13e8961e47edb08b618a5a7b83587cfa5a813873
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2015-12-23 19:21:41 +0200

    Moving things around

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -2,12 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	sortParams: ['published:desc'],
-	sorted: Ember.computed.sort('model', 'sortParams'),
-
-	actions: {
-		deleteArticle (article) {
-			article.deleteRecord();
-			article.save();
-		}
-	}
+	sorted: Ember.computed.sort('model', 'sortParams')
 });
--- app/routes/articles.js
+++ app/routes/articles.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model () {
 		return this.store.findAll('article');
+	},
+	actions: {
+		deleteArticle (article) {
+			article.deleteRecord();
+			article.save();
+		}
 	}
 });

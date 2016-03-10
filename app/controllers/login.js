fdf0d4e0bf6138222ba550b15c8b825e2a604621
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2016-03-10 18:35:36 +0200

    Collect the user credentials

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
 	actions: {
 		login () {
 			console.log(this.get('username'), this.get('password'));
+			let {usename, password} = this.getProperties('username', 'password');
 		}
 	}
 });

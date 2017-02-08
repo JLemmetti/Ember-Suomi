fd31479f0f8aa9d853473be7b0cf3bc0711ba52a
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2017-02-08 21:36:57 +0200

    Hide articles from the article list if they are not released

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
+const {
+	computed
+}  = Ember;
+
 export default Ember.Controller.extend({
 	sortParams: ['published:desc'],
-	sorted: Ember.computed.sort('model', 'sortParams')
+	sorted: Ember.computed.sort('released', 'sortParams'),
+
+	// TODO: Still needs page refres after release state has been changed
+	// for a article
+	released: computed.filter('model', function (article) {
+		return article.get('released');
+	})
 });

166369434068d2b4764c416d68b187eca6f08104
JLemmetti <jurkkalemmetti@Jurkkas-iMac.local>
2017-02-08 19:11:48 +0200

    Fixed deprecation warning

--- app/models/article.js
+++ app/models/article.js
@@ -6,7 +6,9 @@ export default DS.Model.extend({
 	title: DS.attr('string'),
 	lead: DS.attr('string'),
 	body: DS.attr('string'),
-	published: DS.attr('date', {defaultValue: new Date()}),
+	published: DS.attr('date', {defaultValue: function () {
+		return new Date();
+	}}),
 	updated: DS.attr('date'),
 	released: DS.attr('boolean', {defaultValue: false}),
 	rev: DS.attr('string')

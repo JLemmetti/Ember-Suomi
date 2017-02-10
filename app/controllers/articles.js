4f3ccd806e9aff0eed54fa2fe2b6f5daebd1ae1f
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2017-02-10 18:45:40 +0200

    Only show released articles to visitors that have not authenticated

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -6,5 +6,16 @@ const {
 
 export default Ember.Controller.extend({
 	sortParams: ['published:desc'],
-	sortedArticles: Ember.computed.sort('model', 'sortParams')
+	sortedArticles: Ember.computed.sort('articles', 'sortParams'),
+
+	// Only show released articles to visitors that have not authenticated
+	articles: computed('model', 'session.isAuthenticated', {
+		get () {
+			if (this.get('session.isAuthenticated')) {
+				return this.get('model');
+			} else {
+				return this.get('model').filter(article => article.get('released'));
+			}
+		}
+	})
 });

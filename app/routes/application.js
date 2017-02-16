1e8a6f849a54de8b48a1346c3fc8755dbd6c0d04
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2017-02-16 22:32:42 +0200

    Use the logged in user as the model for application

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,5 +8,11 @@ export default Ember.Route.extend({
 	},
 	beforeModel () {
 		return this.get('session').fetch().catch(function() {});
+	},
+	model () {
+		return this.store.query('author', {orderBy: 'firebaseUID', equalTo: this.get('session.uid')})
+			.then(users => {
+				return users.get('firstObject');
+			});
 	}
 });

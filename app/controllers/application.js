b43e17ce0afbc7f4aa0fe6f7591933149c21a2e4
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2017-02-16 20:25:20 +0200

    Login with email and password

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	email: null,
+	password: null,
+
+	actions: {
+		signIn: function (provider) {
+
+			let email = this.get('email'),
+				password = this.get('password');
+
+			this.get('session').open('firebase', {provider, email, password}).then(function (data) {
+				console.log(data.currentUser);
+			});
+		},
+		signOut: function () {
+			this.get('session').close();
+		}
+	}
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -8,15 +8,5 @@ export default Ember.Route.extend({
 	},
 	beforeModel: function() {
 		return this.get('session').fetch().catch(function() {});
-	},
-
-	actions: {
-		signIn: function (provider) {
-			this.get('session').open('firebase', {provider}).then(function (data) {
-			console.log(data.currentUser);
-		});
-		}, signOut: function () {
-			this.get('session').close();
-		}
 	}
 });

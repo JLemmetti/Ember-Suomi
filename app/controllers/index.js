c4c65a16d31fec6133fe7016757246ab13d3df23
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2017-02-17 17:28:17 +0200

    List articles on index route

---
+++ app/controllers/index.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+const {
+	computed
+} = Ember;
+
+export default Ember.Controller.extend({
+	sortParams: ['published:desc'],
+	sortedArticles: Ember.computed.sort('articles', 'sortParams'),
+
+	// Only show released articles to visitors that have not authenticated
+	articles: computed('model', 'session.isAuthenticated', {
+		get () {
+			if (this.get('session.isAuthenticated')) {
+				return this.get('model');
+			} else {
+				return this.get('model').filter(article => article.get('released'));
+			}
+		}
+	})
+});

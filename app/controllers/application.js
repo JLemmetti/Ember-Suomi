59805dd09adff624500c85f6b0d462e9ae45cdd1
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2017-02-19 12:20:49 +0200

    Toggle login form visibility by clicking the logo in the footer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	email: null,
 	password: null,
+	loginFormVisible: false,
 
 	actions: {
 		signIn: function (provider) {
@@ -16,6 +17,9 @@ export default Ember.Controller.extend({
 		},
 		signOut: function () {
 			this.get('session').close();
+		},
+		toggleLoginForm () {
+			this.set('loginFormVisible', !this.get('loginFormVisible'));
 		}
 	}
 });
